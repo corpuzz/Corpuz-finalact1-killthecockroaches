914d76a6406e33fb745e26091263909342ac337f
corpuzz <corpuz.markjanlee@gmail.com>
2025-05-04 02:40:13 +0800

    fix slipper cursor 2

--- js/cursor.js
+++ js/cursor.js
@@ -2,19 +2,19 @@ let cursorAnimationFrame = 0;
 let cursorAnimInterval;
 
 function setNormalCursor() {
-    gameArea.style.cursor = "url('../images/slipper-cursor.png') 50 50, auto";
+    gameArea.style.cursor = "url('/images/slipper-cursor.png') 50 50, auto";
     gameArea.style.transform = 'none';
 }
 
 function setSlapCursor() {
     const frames = [
-        { cursor: "url('../images/slipper-cursor.png')", x: 50, y: 50, rotate: 0 },
-        { cursor: "url('../images/slipper-cursor.png')", x: 45, y: 45, rotate: -15 },
-        { cursor: "url('../images/slipper-cursor.png')", x: 40, y: 40, rotate: -30 },
-        { cursor: "url('../images/slipper-cursor-squash.png')", x: 35, y: 35, rotate: -45 },
-        { cursor: "url('../images/slipper-cursor.png')", x: 40, y: 40, rotate: -30 },
-        { cursor: "url('../images/slipper-cursor.png')", x: 45, y: 45, rotate: -15 },
-        { cursor: "url('../images/slipper-cursor.png')", x: 50, y: 50, rotate: 0 }
+        { cursor: "url('/images/slipper-cursor.png')", x: 50, y: 50, rotate: 0 },
+        { cursor: "url('/images/slipper-cursor.png')", x: 45, y: 45, rotate: -15 },
+        { cursor: "url('/images/slipper-cursor.png')", x: 40, y: 40, rotate: -30 },
+        { cursor: "url('/images/slipper-cursor-squash.png')", x: 35, y: 35, rotate: -45 },
+        { cursor: "url('/images/slipper-cursor.png')", x: 40, y: 40, rotate: -30 },
+        { cursor: "url('/images/slipper-cursor.png')", x: 45, y: 45, rotate: -15 },
+        { cursor: "url('/images/slipper-cursor.png')", x: 50, y: 50, rotate: 0 }
     ];
     
     clearInterval(cursorAnimInterval);
